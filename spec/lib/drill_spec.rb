cfbb84135884ece2eaa50fb2b98cdb810da9e7c7
Mark Wilden <mark@mwilden.com>
2013-11-18 10:26:32 -0800

    Fix spec

--- spec/lib/drill_spec.rb
+++ spec/lib/drill_spec.rb
@@ -4,12 +4,12 @@ describe Drill do
   describe 'recording and reporting results' do
     it 'should tally the number of right and wrong answers and give the percent' do
       drill = Drill.new
-      drill.record true
-      drill.record false
-      drill.record true
-      drill.correct_answers.should == 2
-      drill.wrong_answers.should == 1
-      drill.total_answers.should == 3
+      drill.record_correct_answer
+      drill.record_incorrect_answer
+      drill.record_correct_answer
+      drill.correct_answer_count.should == 2
+      drill.incorrect_answer_count.should == 1
+      drill.total_answer_count.should == 3
       drill.correct_percent.should == 67
     end
   end
