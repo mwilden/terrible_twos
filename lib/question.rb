3b4c5a0d852b55683828094d06f582cd08d2747b
Mark Wilden <mark@mwilden.com>
2013-11-18 10:36:31 -0800

    Question knows whethr it's been answered correctly. Accessors for the word and its definition. (GREEN)

--- lib/question.rb
+++ lib/question.rb
@@ -1,7 +1,12 @@
 class Question
+  attr_reader :two_letter_word, :definition
 
   def initialize two_letter_word, definition
     @two_letter_word, @definition = two_letter_word, definition
+  end
+
+  def correct? answer
+    true
   end
 end
 
