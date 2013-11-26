47493afa0c43582f590a4dbdf1638e2747f635bb
Mark Wilden <mark@mwilden.com>
2013-11-25 22:07:32 -0800

    Debugger

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'http://rubygems.org'
 
 gem 'gem-man'
+gem 'debugger'
 gem 'rake'
 gem 'ronn'
 gem 'rspec'
--- lib/terrible_twos.rb
+++ lib/terrible_twos.rb
@@ -1,3 +1,5 @@
+require 'debugger'
+require_relative 'debugger'
 require 'forwardable'
 require '/Users/mwilden/terrible_twos/lib/drill'
 require '/Users/mwilden/terrible_twos/lib/question'
