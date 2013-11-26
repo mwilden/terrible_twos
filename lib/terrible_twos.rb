7ad43743027aa4daaddf66746d64d3e5641788b9
Mark Wilden <mark@mwilden.com>
2013-11-26 13:25:51 -0800

    Only put absolutely general requires here

--- lib/terrible_twos.rb
+++ lib/terrible_twos.rb
@@ -1,6 +1,4 @@
 # coding: UTF-8
 require 'debugger'
 require_relative 'debugger'
-require 'forwardable'
-require '/Users/mwilden/terrible_twos/lib/drill'
-require '/Users/mwilden/terrible_twos/lib/question'
+require_relative 'lll'
