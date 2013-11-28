3c1636f4473bdd9926311ee42271633bfded7ed6
Mark Wilden <mark@mwilden.com>
2013-11-28 13:05:00 -0800

    Remove commented-out code

--- lib/drill.rb
+++ lib/drill.rb
@@ -14,26 +14,6 @@ class Drill
     @correct_answer_count = @incorrect_answer_count = 0
   end
 
-  #def valid? answer
-    #true
-  #end
-
-  #def record_correct_answer
-    #@correct_answer_count += 1
-  #end
-
-  #def record_incorrect_answer
-    #@incorrect_answer_count += 1
-  #end
-
-  #def total_answer_count
-    #@correct_answer_count + @incorrect_answer_count
-  #end
-
-  #def correct_percent
-    #return 0 if total_answer_count.zero?
-    #(@correct_answer_count.to_f / total_answer_count * 100).round
-  #end
 
   ######
   def create_boards starting_letter, ending_letter
