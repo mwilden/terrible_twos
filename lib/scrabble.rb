3ebc52f60f992c26ea2ad8f7e383f0ea45cb8bd9
Mark Wilden <mark@mwilden.com>
2013-11-27 17:18:17 -0800

    Add rest of a's

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -7,6 +7,20 @@ class Scrabble
     @_two_letter_words ||= [
       TwoLetterWord.new('AA', 'lava'),
       TwoLetterWord.new('AB', 'muscle'),
+      TwoLetterWord.new('AD', 'ad'),
+      TwoLetterWord.new('AE', 'one'),
+      TwoLetterWord.new('AG', 'agriculture'),
+      TwoLetterWord.new('AH', 'interjection'),
+      TwoLetterWord.new('AI', 'three-toed sloth'),
+      TwoLetterWord.new('AL', 'East Indian tree'),
+      TwoLetterWord.new('AM', 'to be'),
+      TwoLetterWord.new('AN', 'indefinite article'),
+      TwoLetterWord.new('AR', 'R'),
+      TwoLetterWord.new('AS', 'to the same degree'),
+      TwoLetterWord.new('AT', 'in the position of'),
+      TwoLetterWord.new('AW', 'interjection'),
+      TwoLetterWord.new('AX', 'ax'),
+      TwoLetterWord.new('AY', 'aye'),
     ]
   end
 
