7f798e2cf233c1f6b75090773d84558ba8f119eb
Mark Wilden <mark@mwilden.com>
2013-11-29 11:40:02 -0800

    --board parameter

--- features/support/step_definitions/steps.rb
+++ features/support/step_definitions/steps.rb
@@ -1,7 +1,7 @@
 require 'greenletters'
 
 When /^I run "(.*?)"$/ do |program_name|
-  step %{a process "#{program_name}" from command "#{program_name}"}
+  step %{a process "#{program_name}" from command "#{program_name} -b Q_"}
   step %{I execute the process "#{program_name}"}
 end
 
